02283a2355186ef1594ec48ba6aca2c13ef7e4c3
Lehasdev <still.prokhorov@gmail.com>
2023-10-31 03:10:48 +1100

    поделил код на функции, изменил проверку csv, добавил привязку через внешний код XML_ID

--- test.php
+++ test.php
@@ -1,91 +1,120 @@
 <?php
-
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php");
-CModule::IncludeModule("iblock"); //Подключение модуля инфоблока
-$csvFile = "test.csv"; // Путь до обрабатываемого файла, в корневой папке
-$filePath = "hash.txt"; // Путь к файлу, где будет храниться хэш
-if (file_exists($csvFile) && is_readable($csvFile)) {//проверка подключаемых файлов
-    $lastHash = file_get_contents($filePath);//подключаю файл с хранящимся хэш кодом с предыдущего запуска скрипта
-    if ($lastHash !== false) {
-        $hash = md5_file($csvFile);//хэширую csv файл
+CModule::IncludeModule("iblock");
 
-        if ($hash === $lastHash) {//проверка на изменение csv файла
-            die(); // Файл не изменился, прерываем выполнение скрипта
-        }
-    }  //файл либо пустой, либо изменен, начинаю создание или обновление
+$csvFile = "test.csv";
+if (file_exists($csvFile) && is_readable($csvFile)) {
+    $dbData = getDatabaseData();
+    $csvData = readCsvData();
+
+    if (areDataDifferent($dbData, $csvData)) {
+        updateDatabase($csvData);
+    }
+}
+
+function isCsvFileValid()
+{
+    global $csvFile;
+    return file_exists($csvFile) && is_readable($csvFile);
+}
+
+function getDatabaseData()
+{
+    $dbData = array();
+
+    $res = CIBlockElement::GetList(array("NAME" => "ASC"), array("IBLOCK_ID" => 4), false, false, array("ID", "XML_ID", "NAME", "PREVIEW_TEXT", "DETAIL_TEXT", "PROPERTY_PROP_1", "PROPERTY_PROP_2"));
+
+    while ($ob = $res->GetNext()) {
+        $dbData[] = $ob;
+    }
+
+    return $dbData;
+}
+
+function readCsvData()
+{
+    global $csvFile;
+    $csvData = array();
     $file = fopen($csvFile, "r");
 
     if ($file !== false) {
+        fgetcsv($file, 0, ";");
 
-        fgetcsv($file, 0, ";");//пропускаю заголовки
-        while (($data = fgetcsv($file, 0, ";")) !== FALSE) {     //открываю цикл и кладу в data построчно
-            list($id, $name, $previewText, $detailText, $prop1, $prop2) = $data; //раскладываю по переменным
-            // пишу проверки пустых значений на входе
-            if (empty($name)) {
-                $name = "unnamed"; // Присвоение значения по умолчанию
-            }
-            if (empty($previewText)) {
-                $previewText = "empty"; // Присвоение значения по умолчанию
-            }
-            if (empty($detailText)) {
-                $detailText = "empty"; // Присвоение значения по умолчанию
-            }
-            if (empty($prop1)) {
-                $prop1 = "empty"; // Присвоение значения по умолчанию
-            }
-            if (empty($prop2)) {
-                $prop2 = "empty"; // Присвоение значения по умолчанию
-            }
-
-            $arFilter = array(     "IBLOCK_ID" => "4",      //id моего инфоблока
-                                   "PROPERTY_MY_ID" => $id //id элемента, добавил свойство
-                              );
-
-            $arSelect = array("ID", "IBLOCK_ID");// что хочу вернуть
-            $res = CIBlockElement::GetList(array("NAME" => "ASC"), $arFilter, false, false, $arSelect);// получаю ресурс для дальнейше работы
-
-
-            $el = new CIBlockElement; //создаю объект класса для работы с элементами инфоблока
-            if ($ob = $res->GetNext()) {
-                // проверяю существует ли элемент, если да - начинаю обновление
-                $arFields = array(
-                    //собираю новые данные
-                    "NAME" => $name,
-                    "PREVIEW_TEXT" => $previewText,
-                    "DETAIL_TEXT" => $detailText,
-                    "PROPERTY_VALUES" => array(
-                        "PROP_1" => $prop1,
-                        "PROP_2" => $prop2,
-                        "MY_ID" => $id
-                    ),
-
-                );
-                $el->Update($ob["ID"], $arFields);
-            } else {
-                // Элемент не существует, создаю новый
-                $arLoadProductArray = array(
-
-                    "IBLOCK_ID" => "4",
-                    "NAME" => $name,
-                    "ACTIVE" => "Y",
-                    "PREVIEW_TEXT" => $previewText,
-                    "DETAIL_TEXT" => $detailText,
-                    "PROPERTY_VALUES" => array(
-                        "PROP_1" => $prop1,
-                        "PROP_2" => $prop2,
-                        "MY_ID"=> $id,
-                    ),
-                );
-                $el->Add($arLoadProductArray);
-            }
+        while (($data = fgetcsv($file, 0, ";")) !== FALSE) {
+            $csvData[] = $data;
         }
         fclose($file);
-
-        if (isset($hash)) {
-            file_put_contents($filePath, $hash); //обновляю хэш запись
-        }
     }
-    BXClearCache(true); //очистка кэша
 
+    return $csvData;
 }
+
+function areDataDifferent($dbData, $csvData)
+{
+    return $dbData !== $csvData;
+}
+
+function updateDatabase($csvData)
+{
+    foreach ($csvData as $data) {
+        processData($data);
+    }
+    BXClearCache(true);
+}
+
+function processData($data)
+{
+    list($id, $name, $previewText, $detailText, $prop1, $prop2) = $data;
+
+    $name = !empty($name) ? htmlspecialcharsbx($name) : "unnamed";
+    $previewText = !empty($previewText) ? htmlspecialcharsbx($previewText) : "empty";
+    $detailText = !empty($detailText) ? htmlspecialcharsbx($detailText) : "empty";
+    $prop1 = !empty($prop1) ? htmlspecialcharsbx($prop1) : "empty";
+    $prop2 = !empty($prop2) ? htmlspecialcharsbx($prop2) : "empty";
+
+    $arFilter = array("IBLOCK_ID" => 4, "XML_ID" => $id);
+    $arSelect = array("ID", "IBLOCK_ID");
+    $res = CIBlockElement::GetList(array("NAME" => "ASC"), $arFilter, false, false, $arSelect);
+    $el = new CIBlockElement;
+
+    if ($ob = $res->GetNext()) {
+        updateExistingElement($el, $ob["ID"], $id, $name, $previewText, $detailText, $prop1, $prop2);
+    } else {
+        addNewElement($el, $id, $name, $previewText, $detailText, $prop1, $prop2);
+    }
+}
+
+function updateExistingElement($el, $elementId, $id, $name, $previewText, $detailText, $prop1, $prop2)
+{
+    $arFields = array(
+        "XML_ID" => $id,
+        "NAME" => $name,
+        "PREVIEW_TEXT" => $previewText,
+        "DETAIL_TEXT" => $detailText,
+        "PROPERTY_VALUES" => array(
+            "PROP_1" => $prop1,
+            "PROP_2" => $prop2,
+        ),
+    );
+    $el->Update($elementId, $arFields);
+}
+
+function addNewElement($el, $id, $name, $previewText, $detailText, $prop1, $prop2)
+{
+    $arLoadProductArray = array(
+        "IBLOCK_ID" => 4,
+        "XML_ID" => $id,
+        "NAME" => $name,
+        "ACTIVE" => "Y",
+        "PREVIEW_TEXT" => $previewText,
+        "DETAIL_TEXT" => $detailText,
+        "PROPERTY_VALUES" => array(
+            "PROP_1" => $prop1,
+            "PROP_2" => $prop2,
+        ),
+    );
+    $el->Add($arLoadProductArray);
+}
+
+
 ?>
