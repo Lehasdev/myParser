3b0d276ce56145dcf89a1873985cb36b1fe1d408
Lehasdev <still.prokhorov@gmail.com>
2023-11-09 01:44:49 +1100

    дополнил функцию сравнения данных, добавил функцию удаления лишнего элемента

--- test.php
+++ test.php
@@ -1,17 +1,20 @@
 <?php
 require ($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php");
 CModule::IncludeModule("iblock");
-const InfoBlockId = 4;
+const InfoBlockId = 4;              //общая константа id инфоблока
 $csvFilePath = "test.csv";
 
-if (file_exists($csvFilePath) && is_readable($csvFilePath)){      //выполняю валидацию и получаю данные для сравнения
+if (file_exists($csvFilePath) && is_readable($csvFilePath)){     //выполняю валидацию и получаю данные для сравнения
     $databaseData= getDatabaseData();
     $csvData= readCsvData();
 
-    if(areDataDifferent($databaseData,$csvData)){
-        updateDatabase($csvData);              //проверка на изменение csv и обновление/создание если true
-    }
-}
+    if(areDataDifferent($databaseData,$csvData)){                //функция сравнения для csv и бд, bool
+        deleteExtraRecords($databaseData, $csvData);           //функция удаляет элемент если его нет в csv
+        updateDatabase($csvData);                              //проверка на изменение csv и обновление/создание если true
+        echo "csv изменился, массивы данных разные";
+
+    } else { echo "csv не изменен, масивы данных одинаковы"; }      //return;
+}return;
 function getDatabaseData()
 {
     $databaseData = array();
@@ -35,7 +38,31 @@ function readCsvData(){
     return $csvData;
 }
 function areDataDifferent($databaseData,$csvData){
-    return $databaseData !== $csvData;
+    $dbTempData = array(); //массив временных данных из бд для сравнения
+    $csvTempData = array();//массив временных данных из csv для сравнения
+
+    $databaseData = array_reverse($databaseData); // пробегаю по элементам и собираю удобный массив. данные из бд идут в другом порядке, переворачиваю.
+    foreach (array_reverse($databaseData) as $item) {
+        $dbTempData[] = [
+            "id" => ($item["XML_ID"] !== "empty") ? $item["XML_ID"] : "",
+            "name" => ($item["NAME"] !== "unnamed") ? $item["NAME"] : "",
+            "preview_text" => ($item["PREVIEW_TEXT"] !== "empty") ? $item["PREVIEW_TEXT"] : "",
+            "detail_text" => ($item["DETAIL_TEXT"] !== "empty") ? $item["DETAIL_TEXT"] : "",
+            "property1" => ($item["PROPERTY_PROP_1_VALUE"] !== "empty") ? $item["PROPERTY_PROP_1_VALUE"] : "",
+            "property2" => ($item["PROPERTY_PROP_2_VALUE"] !== "empty") ? $item["PROPERTY_PROP_2_VALUE"] : "",
+        ];
+    }
+
+    for($i=0;count($csvData)>$i;$i++){
+
+        $csvTempData[$i]["id"] = $csvData[$i][0];
+        $csvTempData[$i]["name"] = $csvData[$i][1];
+        $csvTempData[$i]["preview_text"] = $csvData[$i][2];
+        $csvTempData[$i]["detail_text"] = $csvData[$i][3];
+        $csvTempData[$i]["property1"] = $csvData[$i][4];
+        $csvTempData[$i]["property2"] = $csvData[$i][5];
+    }
+    return $dbTempData !== $csvTempData;
 }
 function updateDatabase($csvData){
     foreach ($csvData as $data){
@@ -52,6 +79,7 @@ function processData($data){
     $property1=!empty($property1)? htmlspecialcharsbx($property1):"empty";
     $property2=!empty($property2)? htmlspecialcharsbx($property2):"empty";
 
+    //поиск элемета по id
     $res = CIBlockElement::GetList(array("NAME" => "ASC"), array("IBLOCK_ID" => InfoBlockId,"XML_ID"=>$id), false, false, array("ID","IBLOCK_ID"));
     $el = new CIBlockElement;
     if($IBlockElement=$res->GetNext()){
@@ -88,5 +116,32 @@ function addNewElement($el,$id,$name,$previewText,$detailText,$property1,$proper
         ),
     );
     $el->Add($arLoadProductArray);
+
+}
+function deleteExtraRecords($databaseData, $csvData)
+{
+    // Получить все id записей из базы данных
+    $databaseIds = array_map(function ($data) {
+        return $data['XML_ID'];
+    }, $databaseData);
+
+    // Получите все id записей из csv-файла
+    $csvIds = array_map(function ($data) {
+        return $data[0];
+    }, $csvData);
+
+    // Найти id записей, которых нет в csv-файле
+    $idsToDelete = array_diff($databaseIds, $csvIds);
+    if(!empty($idsToDelete)){
+
+        // Удалить записи
+        foreach ($idsToDelete as $idToDelete) {
+            $res = CIBlockElement::GetList(array("NAME" => "ASC"), array("IBLOCK_ID" => InfoBlockId, "XML_ID" => $idToDelete), false, false, array("ID", "IBLOCK_ID"));
+            if ($IBlockTempElement = $res->GetNext()) {
+                CIBlockElement::Delete($IBlockTempElement["ID"]);
+            }
+
+        }
+    }
 
 }
